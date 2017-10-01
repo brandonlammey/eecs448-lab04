77ea0eb94dadd1ced74862f940038758e6e7a7fc
Brandon (Archer) Lammey <archerlammey@ku.edu>
2017-10-01 18:43:36 -0500

    Updated multiplicationTable.php
    Added home button

--- multiplicationTable.php
+++ multiplicationTable.php
@@ -36,6 +36,15 @@ echo "<table border=\"5\">";
 
 		}
 		echo'</tr>';
+	  	echo('<table>');
+
+echo('</br>');
+echo('</br>');
+	echo"<a href='index.html'>HOME</a>";
+echo('</br>');
+echo('</br>');
+
+ 	echo('</table>');
   }
   
 ?>
